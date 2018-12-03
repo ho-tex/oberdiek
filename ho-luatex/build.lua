c8fa256e51b69a64d9260303fc6db90535ed1272
David Carlisle <d.p.carlisle@gmail.com>
2018-12-03 19:59:42 +0000

    start at ho-luatex build

---
+++ ho-luatex/build.lua
@@ -0,0 +1,17 @@
+-- Build script for test-oberdiek
+
+-- Build script for oberdiek bundle (check only)
+module = "oberdiek"
+--sourcefiles = {'*.dtx'}
+installfiles = {'*.def','*.sty','*.dfu','oberdiek.luatex.lua', 'luacolor.lua','magicnum.lua','pdftexcmds.lua','bmpsize-test.tex'}
+-- unpackfile = {'oberdiek.ins'}
+
+
+checkconfigs = {"build","config-plain", "config-noxetex","config-luatex","config-pdftex"}
+checkruns = 2
+
+
+kpse.set_program_name ("kpsewhich")
+if not release_date then
+ dofile ( kpse.lookup ("l3build.lua"))
+end
